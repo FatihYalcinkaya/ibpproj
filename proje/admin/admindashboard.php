02776e0d837570f1255b8cbd72da4a6fb599853c
Fatih Yalçınkaya <96224317+FatihYalcinkaya@users.noreply.github.com>
2023-05-30 18:30:30 +0300

    Add files via upload
    biraz degisiklikler yapıldı dashboard temeli atıldı

---
+++ proje/admin/admindashboard.php
@@ -0,0 +1,105 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+        }
+
+        .sidebar {
+            position: fixed;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 220px;
+            background-color: #333;
+            padding-top: 20px;
+        }
+
+        .logo {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .menu li {
+            list-style: none;
+            padding: 15px;
+            border-bottom: 1px solid #444;
+        }
+
+        .menu li:last-child {
+            border-bottom: none;
+        }
+
+        .menu li a {
+            color: #fff;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+        }
+
+        .menu li a i {
+            margin-right: 10px;
+        }
+
+        .main-content {
+            margin-left: 220px;
+            padding: 30px;
+        }
+
+        .header {
+            margin-bottom: 30px;
+        }
+
+        .info {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+        <ul class="menu">
+            <li class="active"><a href="#"><i class="fas fa-home"></i> Dashboard</a></li>
+            <li><a href="./useroperations/userindex.php"><i class="fas fa-users"></i> User Operations</a></li>
+            <li><a href="logout.php"><i class="fas fa-book"></i> Book Operations</a></li>
+            <li><a href="announcement.php"><i class="fas fa-envelope"></i> Announcement</a></li>
+            <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+
+        </ul>
+    </div>
+    <div class="main-content">
+        <div class="header">
+            <h1>Admin Panel</h1>
+        </div>
+        <div class="info">
+            <p>This is a sample admin panel created using HTML and CSS.</p>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ proje/admin/bookoperations/bookview.php
@@ -0,0 +1,105 @@
+<!-- dbdeki kitap bilgilerini gösterme -->
+
+
+<?php
+
+require 'dbconnection.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>User Edit</title>
+</head>
+
+<body>
+
+    <div class="container mt-5">
+
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card-header">
+
+                    <h4>Book View Details
+                        <a href="index.php" class="btn btn-danger float-end">BACK</a>
+                    </h4>
+
+                </div>
+                <div class="card-body">
+
+                    <?php
+                    if (isset($_GET['id'])) {
+
+                        $bookID = mysqli_real_escape_string($connection, $_GET['id']);
+                        $query = "SELECT * FROM book WHERE id='$bookID'";
+
+                        $query_run = mysqli_query($connection, $query);
+
+                        // var mı yok mu kontrol
+
+                        if (mysqli_num_rows($query_run) > 0) {
+                            $book = mysqli_fetch_array($query_run);
+
+                    ?>
+
+
+
+                            <div class="mb-3">
+                                <label for="">Name</label>
+
+                                <p class="form-control">
+                                    <?= $book['name']; ?>
+                                </p>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="">Author</label>
+
+                                <p class="form-control">
+                                    <?= $book['author']; ?>
+                                </p>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="">Summary</label>
+
+                                <p class="form-control">
+                                    <?= $book['summary']; ?>
+
+                                </p>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="">Status</label>
+
+                                <p class="form-control">
+                                    <?= $book['status']; ?>
+                                </p>
+                            </div>
+
+
+                    <?php
+
+                        } else {
+                            echo "<h4>No ID Found!! </h4>";
+                        }
+                    }
+                    ?>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
